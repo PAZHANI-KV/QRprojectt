36a385c741ab3a7d054be3c323425513f54ec4ba
PAZHANI-KV <pazhanivel11111@gmail.com>
2024-02-16 13:36:17 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import Login from "./Login";
+import Signup from "./signup";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Home from './Home'
+
+
+
+
+const App = () => {
+  return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Home/>}></Route>
+  <Route path="/register" element={<Signup/>}></Route>
+  <Route path="/login" element={<Login/>}></Route>
+  <Route path="/dashboard" element={<Dashboard/>}></Route>
+
+
+  </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
